1a1622e35415fd30752bb1186645a71f26de2de8
Scot Newbury <scot@scotnewbury.com>
2026-06-03 10:41:34 -0400

    feat: added username length check

--- validator.py
+++ validator.py
@@ -1,6 +1,8 @@
-def validate_username():
-    print("This is the way")
+def validate_username(username: str) -> bool:
+    return 3 <= len(username) <= 20
 
 
 if __name__ == "__main__":
-    validate_username()
+    print(validate_username("scot"))
+    print(validate_username("so12345678901234567890"))
+    print(validate_username(""))
