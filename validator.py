7610a19d9c9a85b67992271e5cb678622f9b9286
Scot Newbury <scot@scotnewbury.com>
2026-06-05 11:40:51 -0400

    refactor: added several validation rules, changed initial logic

--- validator.py
+++ validator.py
@@ -1,8 +1,16 @@
 def validate_username(username: str) -> bool:
-    return 3 <= len(username) <= 20
+    if not (3 <= len(username) <= 20):
+        return False
+    elif username == "":
+        return False
+    elif username[0].isdigit():
+        return False
+    else:
+        return True
 
 
 if __name__ == "__main__":
     print(validate_username("scot"))
-    print(validate_username("so12345678901234567890"))
+    print(validate_username("A12345678901234567890"))
     print(validate_username(""))
+    print(validate_username("2fortea"))
