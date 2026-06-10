2228ffd4fc1f18ab504762d9f15b4b2902c68646
Scot Newbury <scot@scotnewbury.com>
2026-06-10 07:18:18 -0400

    feature: added test for characters outside legal values

--- validator.py
+++ validator.py
@@ -3,6 +3,9 @@ def validate_username(username: str) -> bool:
         return False
     elif username[0].isdigit():
         return False
+    # elif all(c.isascii() and c.isalnum() or c == "_"):
+    elif not all(char.isascii() and char.isalnum() or char == "_" for char in username):
+        return False
     else:
         return True
 
@@ -12,4 +15,7 @@ if __name__ == "__main__":
     print(f"Userrname too long: {validate_username('A12345678901234567890')}")
     print(f"Username empty: {validate_username('')}")
     print(f"Username starts with a number: {validate_username('2fortea')}")
-    print(f"Username has an invalid character: {validate_username('abc$123')}")
+    print(
+        f"Username, abc$123, has an invalid character: {validate_username('abc$123')}"
+    )
+    print(f"Unsername, def_456, is legal: {validate_username('def_456')}")
