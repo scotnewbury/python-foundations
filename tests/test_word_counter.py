d4130e672b4567472b39d9e53c608bea84e6f256
Scot Newbury <scot@scotnewbury.com>
2026-08-01 15:32:04 -0400

    test: add count_words tests

--- tests/test_word_counter.py
+++ tests/test_word_counter.py
@@ -1,2 +1,33 @@
-def test_place_holder():
-    pass
+from word_counter import (
+    count_words,
+)
+
+
+class TestCountWords:
+    def test_count_words_strips_punctuation(self):
+        text = "May the force be with you, always!"
+        result = count_words(text)
+        assert all(
+            key == key.strip(".,!?;:'\"") for key in result.keys()
+        ), "Not all keys are stripped of punctuation"
+
+    def test_count_words_excludes_empty_keys(self):
+        text = ".,!?;:'\""
+        result = count_words(text)
+        assert result == {}
+
+    def test_count_words_is_lowercase(self):
+        text = "This is The Way"
+        result = count_words(text)
+        assert all(
+            key == key.lower() for key in result.keys()
+        ), "Not all keys are lowercase"
+
+    def test_count_words_basic(self):
+        text = "the cat sat on the mat"
+        result = count_words(text)
+        assert result["the"] == 2
+        assert result["cat"] == 1
+
+    def test_count_words_empty(self):
+        assert count_words("") == {}
--- word_counter.py
+++ word_counter.py
@@ -23,6 +23,37 @@ def read_text(path: str) -> str:
     return content
 
 
+def count_words(text: str) -> dict[str, int]:
+    """Read a string and return a dictionary of words and their counts.
+
+    The string is normalized by making it lowercase and removing edge punctuation.
+
+    Words that are normalized to nothing are excluded from the count.
+
+    Args:
+        text: The string containing the words to be counted
+
+    Returns:
+        A dictionary of keys and their counts, or an empty dictionary.
+    """
+    if text == "":
+        return {}
+
+    word_dict: dict[str, int] = {}
+    word_list = text.lower().split()
+
+    for word in word_list:
+        word = word.strip(".,!?;:'\"")
+        if word:
+            word_dict[word] = word_dict.get(word, 0) + 1
+
+    return word_dict
+
+
+# print(count_words("The cat didn't sat on the mat, mat"))
+
+print("Second test: ", count_words(".,!?;:'\""))
+
 if __name__ == "__main__":
     if len(sys.argv) > 1:
         print(read_text(sys.argv[1]))
